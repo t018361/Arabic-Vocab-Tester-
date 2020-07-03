ff3c1265d6731a3ea4c2bd17c6c70cfd2155923f
t018361 <t0183619@gmail.com>
2020-07-03 07:53:30 +0000

    Tidying up

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def arabic_text(text):
   return bidi_text
 
 def incorrect_function(tple, incorrect_list):
-  incorrect_list_prompt = input('\nWould you like to add \'{}\' to your incorrect list? (yes/no)\n'.format(arabic_text(tple[0])))
+  incorrect_list_prompt = input('\nWould you like to add \'{}\' to your review list? (yes/no)\n'.format(arabic_text(tple[0])))
   yes_command = (incorrect_list_prompt == 'yes' or incorrect_list_prompt == 'Yes' or incorrect_list_prompt == 'yes ' or incorrect_list_prompt == 'Yes ')
   if yes_command:
     arabic = arabic_text(tple[0])
@@ -42,10 +42,8 @@ def complete_test(lst):
 
   return
 
-def random_test(lst): 
+def random_test(lst, total_questions): 
   incorrect_list = []
-  question_number_input = input('How many questions would you like?\n')
-  total_questions = int(question_number_input)
   question_number = 1
   while question_number <= total_questions: 
     index_position = random.choice((range(len(lst))))
@@ -54,15 +52,27 @@ def random_test(lst):
     question_number += 1
   if len(incorrect_list) > 0:
     words = pd.DataFrame(incorrect_list, columns = ['Arabic', 'English'])
-    print('\nThe word(s) that you got incorrect were: \n')  
+    print('\nThe word(s) that you selected were: \n')  
     print(words)
   
-    retest = input('\nWould you like a test on the words which you got wrong? (yes/no)\n')
+    retest = input('\nWould you like to review the words you selected? (yes/no)\n')
     yes_command = (retest == 'yes' or retest == 'Yes' or retest == 'yes ' or retest == 'Yes ')
     if yes_command:
       print(' --------------------\n')
       complete_test(incorrect_list)
+      print('\nHere is your review list again:\n')
+      print(words)
+  return
+
+def setup():
+  print('Which of the following lists would you like to be tested on?\n')
+  print(' - chapter 1 roots vocab list (1a)\n')
+  vocab_list = input('Please type the list key (eg. 1a).\n')
+  question_number_input = input('How many questions would you like?\n')
+  total_questions = int(question_number_input)
+  if str(vocab_list) == '1a' or str(vocab_list) == '1a ':
+    random_test(chapter1_roots_vocab_list, total_questions)
   return
 
 
-random_test(chapter1_roots_vocab_list)
+setup()
